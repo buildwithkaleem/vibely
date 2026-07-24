e52c491195a20058aa22d2deabd00d7dcbb77409
ranakaleem786 <ranakaleem786422@gmail.com>
2026-07-24 17:41:19 +0500

    8 commit

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -13,8 +13,8 @@
 
 
 import User from "../models/User.js";
-import generateToken from "../utils/generateToken.js";
-import { getTikTokUser } from "../services/tiktok.service.js";
+// import generateToken from "../utils/generateToken.js";
+// import { getTikTokUser } from "../services/tiktok.service.js";
 import crypto from "crypto";
 import axios from "axios";
 
@@ -38,52 +38,11 @@ export const login = async (req, res) => {
 
 
 
-export const callback = async (req, res) => {
-  try {
-    const { code } = req.query;
-
-    // 1. Authorization Code → Access Token
-    const response = await axios.post(
-      "https://open.tiktokapis.com/v2/oauth/token/",
-      new URLSearchParams({
-        client_key: process.env.TIKTOK_CLIENT_KEY,
-        client_secret: process.env.TIKTOK_CLIENT_SECRET,
-        code,
-        grant_type: "authorization_code",
-        redirect_uri: process.env.TIKTOK_REDIRECT_URI,
-      }),
-      {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      }
-    );
-
-    // 2. Access Token se TikTok User Profile lao
-    const profile = await getTikTokUser(response.data.access_token);
-
-    console.log("========== PROFILE ==========");
-    console.log(profile);
-
-    // 3. Browser ko response bhejo
-    return res.json({
-      token: response.data,
-      profile,
-    });
-
-  } catch (err) {
-    console.error(err.response?.data || err.message);
-
-    return res.status(500).json(
-      err.response?.data || { error: err.message }
-    );
-  }
-};
-
 // export const callback = async (req, res) => {
 //   try {
 //     const { code } = req.query;
 
+//     // 1. Authorization Code → Access Token
 //     const response = await axios.post(
 //       "https://open.tiktokapis.com/v2/oauth/token/",
 //       new URLSearchParams({
@@ -100,19 +59,60 @@ export const callback = async (req, res) => {
 //       }
 //     );
 
-//     const profile = await getTikTokUser(
-//       response.data.access_token
-//     );
+//     // 2. Access Token se TikTok User Profile lao
+//     const profile = await getTikTokUser(response.data.access_token);
 
+//     console.log("========== PROFILE ==========");
 //     console.log(profile);
 
-//     return res.json(response.data);
+//     // 3. Browser ko response bhejo
+//     return res.json({
+//       token: response.data,
+//       profile,
+//     });
+
 //   } catch (err) {
 //     console.error(err.response?.data || err.message);
 
-//     return res.status(500).json(err.response?.data || { error: err.message });
+//     return res.status(500).json(
+//       err.response?.data || { error: err.message }
+//     );
 //   }
 // };
+
+export const callback = async (req, res) => {
+  try {
+    const { code } = req.query;
+
+    const response = await axios.post(
+      "https://open.tiktokapis.com/v2/oauth/token/",
+      new URLSearchParams({
+        client_key: process.env.TIKTOK_CLIENT_KEY,
+        client_secret: process.env.TIKTOK_CLIENT_SECRET,
+        code,
+        grant_type: "authorization_code",
+        redirect_uri: process.env.TIKTOK_REDIRECT_URI,
+      }),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+
+    const profile = await getTikTokUser(
+      response.data.access_token
+    );
+
+    console.log(profile);
+
+    return res.json(response.data);
+  } catch (err) {
+    console.error(err.response?.data || err.message);
+
+    return res.status(500).json(err.response?.data || { error: err.message });
+  }
+};
 
 // export const callback = async (req, res) => {
 
--- src/services/tiktok.service.js
+++ src/services/tiktok.service.js
@@ -25,38 +25,38 @@
 
 
 
-import axios from "axios";
+// import axios from "axios";
 
-export const getTikTokUser = async (accessToken) => {
-  try {
-    const response = await axios.post(
-      "https://open.tiktokapis.com/v2/user/info/",
-      {
-        fields: [
-          "open_id",
-          "union_id",
-          "avatar_url",
-          "display_name",
-          "username",
-        ],
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
+// export const getTikTokUser = async (accessToken) => {
+//   try {
+//     const response = await axios.post(
+//       "https://open.tiktokapis.com/v2/user/info/",
+//       {
+//         fields: [
+//           "open_id",
+//           "union_id",
+//           "avatar_url",
+//           "display_name",
+//           "username",
+//         ],
+//       },
+//       {
+//         headers: {
+//           Authorization: `Bearer ${accessToken}`,
+//           "Content-Type": "application/json",
+//         },
+//       }
+//     );
 
-    console.log("TikTok User API Response:");
-    console.log(response.data);
+//     console.log("TikTok User API Response:");
+//     console.log(response.data);
 
-    return response.data.data;
-  } catch (error) {
-    console.error(
-      "TikTok User Error:",
-      error.response?.data || error.message
-    );
-    throw error;
-  }
-};
+//     return response.data.data;
+//   } catch (error) {
+//     console.error(
+//       "TikTok User Error:",
+//       error.response?.data || error.message
+//     );
+//     throw error;
+//   }
+// };
