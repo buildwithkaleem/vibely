f5c6dd9ab883e51b70773523e9735c51e947b492
ranakaleem786 <ranakaleem786422@gmail.com>
2026-07-26 12:25:52 +0500

    29 commit

--- src/services/tiktok.upload.service.js
+++ src/services/tiktok.upload.service.js
@@ -6,7 +6,8 @@ export const createTikTokDraft = async (
 ) => {
 
   const response = await axios.post(
-    "https://open-api.tiktok.com/v2/post/publish/inbox/video/init/",
+    // "https://open-api.tiktok.com/v2/post/publish/inbox/video/init/",
+    "https://open-api.tiktok.com/v2/post/publish/video/init/",
     {
       source_info: {
         source: "PULL_FROM_URL",
